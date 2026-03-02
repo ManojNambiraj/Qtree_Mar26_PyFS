0874c2aba4113fb3a397a06224c3df1d0d394288
manojnambiraj <postboxmano@gmail.com>
2026-03-02 19:52:47 +0530

    Py intro

---
+++ Python/Class1/intro.py
@@ -0,0 +1,24 @@
+# Output stmt:
+
+    # print("Hello world", end="\n")
+    # print("Good evening...!")
+
+    # print("11", "3", "2000", sep="/")
+
+# Variable:
+
+    # age = 10
+
+    # print(age)
+
+# Datatypes:
+
+    # a = 10 --> int
+    # a = 10.5555
+    # a = 3j --> complex
+    # a = "Hello"
+    # a = True
+    # a = None
+
+    # print(a)
+    # print(type(a))
