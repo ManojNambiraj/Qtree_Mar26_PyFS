faa6f05c5b7b5570c095a0de33583338df8d25c3
manojnambiraj <postboxmano@gmail.com>
2026-03-03 19:43:29 +0530

    Python Operators, Control stmts

---
+++ Python/Class2/operators.py
@@ -0,0 +1,124 @@
+# Operators:
+
+    # Arithmetic operators (+, -, *, /, %, **, //)
+
+        # a = 7
+        # b = 5
+
+        # result = a + b
+        # result = a - b
+        # result = a * b
+        # result = a / b
+        # result = a % b
+        # result = a ** b
+        # result = a // b
+
+        # print(result)
+
+    # Assignment operators (=, +=, -=, *=, /=, %=, **=, //=)
+
+    # Comparison operators (==, !=, >, <, >=, <=)
+
+    # Logical operators (and, or, not)
+
+        # age = 22
+
+        # result = (age > 18) and (age < 30)
+        # result = (age > 18) or (age > 30)
+
+        # print(not(result))
+
+    # Identity operators (is, is not)
+
+        # x = ["apple", "banana", "orange"]
+        # y = ["apple", "banana", "orange"]
+        # z = x
+
+        # print(x is z)
+
+    # Membership operators (in, not in)
+
+        # x = ["apple", "banana", "orange"]
+
+        # print("grapes" not in x)
+
+# Control statement
+
+    # Conditional stmts
+        # if
+
+            # age = 19
+
+            # if(age > 18):
+            #     print("Eligible for voting")
+
+        # if else
+
+            # age = 19
+
+            # if(age > 18):
+            #     print("Eligible for voting")
+            # else:
+            #     print("Not Eligible")
+
+        # elif
+
+            # age = 18
+
+            # if(age > 18):
+            #     print("Eligible for voting")
+            # elif(age == 18):
+            #     print("Partially eligible")
+            # else:
+            #     print("Not Eligible")
+
+    # Looping stmts
+        # for
+
+        # Ex: 1
+
+            # for i in range(3, 11, 2):
+            #     print(i)
+
+        # Ex: 2
+
+            # fruits = ["apple", "banana", "orange"]
+
+            # for item in fruits:
+            #     print(item)
+
+        # while
+
+            # i = 1
+
+            # while(i <= 20):
+            #     print(i)
+            #     i += 2
+
+    # Jumping stmts
+        # break
+
+            # i = 1
+
+            # while(i <= 5):
+
+            #     if(i == 3):
+            #         break
+
+            #     print(i)
+            #     i += 1
+
+        # continue
+
+            # i = 0
+
+            # while(i < 5):
+
+            #     i += 1
+
+            #     if(i == 3):
+            #         continue
+
+            #     print(i)
+    
+
