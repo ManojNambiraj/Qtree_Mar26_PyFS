b4066b01a358840e8f927821c5f5d8085ff53d62
manojnambiraj <postboxmano@gmail.com>
2026-03-05 19:32:10 +0530

    List

---
+++ Python/Class3/Collections.py
@@ -0,0 +1,35 @@
+# Collection Frameworks:
+
+    # List
+    # Tuple
+    # Dictionary
+    # Set
+
+# List
+fruits = ["apple", "orange", "banana", True, 200, 5.5]
+
+#           0         1         2        3    4    5     --> forward indexing
+#                                        -3  -2  -1      --> Backward indexing
+
+# print(fruits)
+# print(type(fruits))
+# print(len(fruits))
+
+# fruits.append(10000)
+# fruits.insert(1, "Grapes")
+# fruits.pop()
+# fruits.pop(1)
+# print(fruits)
+
+# print(fruits[3])
+# print(fruits[-2])
+# print(fruits[1:2])
+
+# print(fruits.index("banana") - len(fruits))
+
+# fruits.extend(["grapes", "watermelon", "dragon"])
+
+# print(fruits)
+
+# for item in fruits:
+#     print(item)
