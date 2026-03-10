7dcd3d1ad30cb87c36fbb142857520e431289e7f
manojnambiraj <postboxmano@gmail.com>
2026-03-10 19:54:00 +0530

    Python Collections

---
+++ Python/Class4/Tuple.py
@@ -0,0 +1,52 @@
+# Tuple:
+
+    # fruits = ("Apple", "Orange", "Banana", "Grapes")
+
+    # print(fruits)
+    # print(type(fruits))
+    # print(fruits[0])
+    # print(fruits[-1])
+    # print(fruits[1:3])
+    # print(len(fruits))
+
+    # for item in fruits:
+    #     print(item)
+
+# Dict
+
+    # student1 = {
+    #     "name": "Raj", 
+    #     "age": 25, 
+    #     "mobile": 987654321, 
+    #     "email": "raj@gmail.com"
+    # }
+
+    # print(student1)
+    # print(type(student1))
+    # print(len(student1))
+    # print(student1["name"])
+
+    # student1["dept"] = "EEE"
+    # student1["name"] = "Raj kumar"
+
+    # student1.pop("mobile")
+    # student1.popitem()
+    # student1.clear()
+
+    # print(student1)
+
+    # for item in student1:
+    #     print(student1[item])
+
+# Set:
+
+    # student = {"Raj", 23, "raj@gmail.com", 9876543232}
+
+    # print(student)
+    # print(type(student))
+
+    # student.add("EEE")
+    # student.pop()
+    # student.clear()
+
+    # print(student)
