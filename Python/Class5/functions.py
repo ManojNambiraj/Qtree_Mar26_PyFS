5ddcdeb00d798320cbe8e48356aa65dab843310b
manojnambiraj <postboxmano@gmail.com>
2026-03-11 19:52:32 +0530

    python functions

---
+++ Python/Class5/functions.py
@@ -0,0 +1,25 @@
+# Functions
+
+    # def mul(n):
+    #     return n * 2
+
+    # result = mul(50)
+
+    # print(result)
+
+# Lambda Function
+
+    # demo = lambda a, b, c, d : a * b * c * d
+
+    # print(demo(2, 7, 4, 8))
+
+# Types od Arguments (or) Parameters
+
+    # Positional Arguments
+    # Default Arguments
+    # Keyword Arguments
+
+def demo(a, b, c=1):
+    print(a, b, c)
+
+demo(b = 2, c = 7, a = 9)
