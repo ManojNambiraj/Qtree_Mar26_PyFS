fb148614dcb5458fd224e0762937f99e455d6c5a
manojnambiraj <postboxmano@gmail.com>
2026-04-13 19:59:55 +0530

    Django Templates

--- Django/home/views.py
+++ Django/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
 
 def index(request):
-    # return HttpResponse("Hello, world. Good evening...!")
     return render(request, 'index.html')
+
+def details(request):
+    return render(request, 'details.html')
